9bea2d2fa279ad62e7a3a1e86398e4d9a0dac1dd
MukendiMputu <mputusam@live.com>
2020-04-12 15:36:01 -0600

    testing redirection

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,5 +1,18 @@
 class DemoController < ApplicationController
   def index
     @array = [1, 2, 3, 4, 5]
+
+    render(:template => 'demo/index') 
+  # render('demo/index') or just
+  # render('index')
+  end
+
+  def other_index
+    render(:template => 'demo/other_index')
+  end
+
+  def redir
+    redirect_to(:controller => 'demo', :action => 'other_index')
+  # redirect_to("www.google.com")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  get 'demo/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Default route 
+  #get ':controller(/:action(/:id))'
+
+  root 'demo#index'
+
+  get 'demo/index' # match "demo/index", :to => "demo#index", :via => :get
+  match "demo/redir", :to => "demo#redir", :via => :get
+  get 'demo/other_index'
+  
 end
