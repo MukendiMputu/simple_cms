35b4813edeebe8680ed1d5c47c4670c87c075936
MukendiMputu <mputusam@live.com>
2020-04-12 16:44:16 -0600

    destroyed demo controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # Default route 
   #get ':controller(/:action(/:id))'
 
-  root 'demo#index'
+  #root 'demo#index'
 
-  get 'demo/index' # match "demo/index", :to => "demo#index", :via => :get
-  match "demo/redir", :to => "demo#redir", :via => :get
-  get 'demo/other_index'
+  #get 'demo/index' # match "demo/index", :to => "demo#index", :via => :get
+  #match "demo/redir", :to => "demo#redir", :via => :get
+  #get 'demo/other_index'
   
 end
