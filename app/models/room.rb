49be847537b9e0a1072d8cc1d14628536d15c312
MukendiMputu <mputusam@live.com>
2020-04-13 12:43:23 -0600

    defined lambda (scope) for ActiveRecords

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,5 @@
 class Room < ApplicationRecord
+    scope :oldest_first, lambda { order("created_at ASC") }
+    scope :newest_first, lambda { order("created_at DESC") }
+    scope :search, ->(query) { where(["number LIKE ?", "%#{query}%"])}
 end
