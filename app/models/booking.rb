2579bda66d7def2eaaaf087ea4aa4c62c5cae0a5
MukendiMputu <mputusam@live.com>
2020-04-13 15:59:59 -0600

    made model association/relationship

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,9 @@
 class Booking < ApplicationRecord
+    # DB Associations: one-to-many, one-to-one
+    belongs_to :user
+    belongs_to :room
+    has_many :notes
+
     scope :oldest_first, lambda { order("created_at ASC") }
     scope :newest_first, lambda { order("created_at DESC") }
     scope :search, ->(query) { where(["agenda LIKE ?", "%#{query}%"])}
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,7 @@
 class Room < ApplicationRecord
+    has_many :bookings
+
+    
     scope :oldest_first, lambda { order("created_at ASC") }
     scope :newest_first, lambda { order("created_at DESC") }
     scope :search, ->(query) { where(["number LIKE ?", "%#{query}%"])}
