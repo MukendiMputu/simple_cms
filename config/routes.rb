4b91547dd62a7d66d8cb622d86c0321de1116ce2
MukendiMputu <mputusam@live.com>
2020-04-13 18:48:09 -0600

    created users_controller and appropiate views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/signup'
+  get 'users/signin'
+  get 'users/show'
+  get 'users/edit'
   # Default route 
   #get ':controller(/:action(/:id))'
   
---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  test "should get signup" do
+    get users_signup_url
+    assert_response :success
+  end
+
+  test "should get signin" do
+    get users_signin_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get users_show_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get users_edit_url
+    assert_response :success
+  end
+
+end
