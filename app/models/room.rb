e8b4cf40d166d51ab93845eb5f445cb9dda5a229
MukendiMputu <mputusam@live.com>
2020-04-13 21:00:21 -0600

    added a new scope to retrieve room ordered by number

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,7 @@
 class Room < ApplicationRecord
     has_many :bookings
 
-    
+    scope :ordered, lambda { order("number") }    
     scope :oldest_first, lambda { order("created_at ASC") }
     scope :newest_first, lambda { order("created_at DESC") }
     scope :search, ->(query) { where(["number LIKE ?", "%#{query}%"])}
