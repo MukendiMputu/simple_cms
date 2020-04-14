aec6c8140f8220e25f4675c1da1dc173f2bb0741
MukendiMputu <mputusam@live.com>
2020-04-13 21:00:40 -0600

    used resourcefull routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
-  get 'users/signup'
-  get 'users/signin'
-  get 'users/show'
-  get 'users/edit'
-  # Default route 
-  #get ':controller(/:action(/:id))'
   
   root 'welcome#index'
+
+  resources :users do
+    member do
+      post :signup
+      post :signin
+      get :delete
+    end
+  end
   
-  get 'welcome/index'
+  
+  
+  #get 'welcome/index'
   #get 'welcome/index' # match "welcome/index", :to => "welcome#index", :via => :get
   
+  # Default route 
+  #get ':controller(/:action(/:id))'
 end
