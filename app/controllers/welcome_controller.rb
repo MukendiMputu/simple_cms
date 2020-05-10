9737d0f084e2164bd4ceeed1079ca8bfebb867d5
MukendiMputu <mputusam@live.com>
2020-05-10 08:44:02 +0200

    added 'booked by' on main page with name and picture

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   
   def index
     @users = User.all
-    @bookings = Booking.all
+    @bookings = Booking.newest_first
     @rooms = Room.ordered
     
   end
