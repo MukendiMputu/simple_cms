26c49204cf04f1f0dc437f60bf690063246ac80a
MukendiMputu <mputusam@live.com>
2020-05-17 08:27:48 +0200

    fixed modal bug: needs to be outside any fixed element

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,12 +24,4 @@ require("channels")
 import 'bootstrap'
 
 
-$('#login_main').on('shown.bs.modal', function () {
-    $('#email').trigger('focus')
-    var button = $(event.relatedTarget) // Button that triggered the modal
-    var recipient = 'user '
-    var modal = $(this)
-    modal.find('.modal-title').text('Login ' + recipient)
-    modal.find('.modal-body input').val(recipient)
-})
 
