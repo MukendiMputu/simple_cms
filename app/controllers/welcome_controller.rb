7c359e0c7120e86acde1605646c0cf97382455c4
MukendiMputu <mputusam@live.com>
2020-06-12 23:43:53 +0200

    refine scope

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,8 +8,9 @@ class WelcomeController < ApplicationController
   
   def search_booking
     sleep(1)
+    
+    # Fetch all the bookings matching the date OR the room number
     @bookings = Booking.find_closest_match(params[:date], params[:number]).order_by_start
-    # @bookings = Booking.date_is(params[:date]).room_is(params[:number])
 
     respond_to do |format|
       format.js
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,13 +6,13 @@ class Booking < ApplicationRecord
     has_many :invitations
     has_many :guests, :through => :invitations
 
-
+    # Quick SQL Queries
+    scope :order_by_end, lambda { order("end_time ASC") }
     scope :oldest_first, lambda { order("created_at ASC") }
     scope :newest_first, lambda { order("created_at DESC") }
-    scope :search, ->(query) { where(["agenda LIKE ?", "%#{query}%"])}
-    scope :date_is, ->(query) { where("DATE(date) = ?", "#{query}")}
-    scope :find_closest_match, ->(q_date, q_room) { where("DATE(date) = ? OR room_id = ?", "#{q_date}", "#{q_room}")}
     scope :order_by_start, lambda { order("start_time ASC") }
-    scope :order_by_end, lambda { order("end_time ASC") }
+    scope :date_is, ->(query) { where("DATE(date) = ?", "#{query}")}
+    scope :search, ->(query) { where(["agenda LIKE ?", "%#{query}%"])}
+    scope :find_closest_match, ->(q_date, q_room) { where("DATE(date) = ? OR room_id = ?", "#{q_date}", "#{q_room}")}
 
 end
