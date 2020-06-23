164af7fb16e52f2093b9d1b7997be3404959044a
MukendiMputu <mputusam@live.com>
2020-06-23 23:35:07 +0200

    add js toggle function

--- app/javascript/channels/tab_channel.js
+++ app/javascript/channels/tab_channel.js
@@ -2,3 +2,12 @@ $('#myTab a').on('click', function (e) {
     e.preventDefault()
     $(this).tab('show')
 })
+
+$('#editBtn').on('click', function (e) {
+    var x = $('#editDiv');
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+})
