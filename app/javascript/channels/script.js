5b17e687a53769f4844d4439f14d3f3b65df4b4a
MukendiMputu <mputusam@live.com>
2020-08-21 09:34:24 +0200

    basic AJAX

---
+++ app/javascript/channels/script.js
@@ -0,0 +1,22 @@
+// Quick summary of the steps to do for implementing a
+// basic AJAX-Request.
+
+var data;
+
+// 1. 
+var request = new XMLHttpRequest();
+
+// 2. 
+request.open('GET', '/root/path/to/document.json');
+
+// 3.
+request.onreadystatechange = function () {
+    // check wether the request status is ok
+    if (request.status === 200 && request.readyState === 4) {
+        // Parse the responce text into a json-object
+        data = JSON.parse(request.responseText);
+    }
+}
+
+// 4.
+request.send();
